946ef8df65a78a60af2ac9dc1f2bae740899abeb
carteras <adam.carter@ed.act.edu.au>
2017-09-17 19:11:05 +1000

    Introduction to JavaScript applied to web develoment.

---
+++ JavaScript/IntroductionToJavaScript/02ExternalJavaScript.js
@@ -0,0 +1,5 @@
+document.getElementById("example_1").innerHTML = "Hello world";
+
+function see(){
+    document.getElementById("example_2").innerHTML = "See!"
+}
---
+++ JavaScript/IntroductionToJavaScript/03ChangeHTMLAttributes.js
@@ -0,0 +1,3 @@
+function change() {
+    document.getElementById("image").src = "img/landscape.jpg";
+}
