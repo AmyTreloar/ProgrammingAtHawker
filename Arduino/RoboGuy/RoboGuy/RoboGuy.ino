7866b7a690cfaac4e86ad43a2a906b9e61dc208c
carteras <adam.carter@ed.act.edu.au>
2017-11-09 18:01:48 +1100

    roboguy

---
+++ Arduino/RoboGuy/RoboGuy/RoboGuy.ino
@@ -0,0 +1,55 @@
+#include <SoftI2cMaster.h>
+#include <SHDefines.h>
+#include <MsTimer2.h>
+#include <EVs_SumoEyes.h>
+#include <EVs_RTC.h>
+#include <EVs_PSPNx.h>
+#include <EVs_PiLight.h>
+#include <EVs_PFMate.h>
+#include <EVs_NXTVoltMeter.h>
+#include <EVs_NXTTouch.h>
+#include <EVs_NXTThermometer.h>
+#include <EVs_NXTServo.h>
+#include <EVs_NXTMMX.h>
+#include <EVs_NXTLight.h>
+#include <EVs_NXTCurrentMeter.h>
+#include <EVs_NXTColor.h>
+#include <EVs_NXTCam.h>
+#include <EVs_NumericPad.h>
+#include <EVs_MagicWand.h>
+#include <EVs_LineLeader.h>
+#include <EVs_LightSensorArray.h>
+#include <EVs_IRThermometer.h>
+#include <EVs_EV3Ultrasonic.h>
+#include <EVs_EV3Touch.h>
+#include <EVs_EV3SensorMux.h>
+#include <EVs_EV3Infrared.h>
+#include <EVs_EV3Gyro.h>
+#include <EVs_EV3Color.h>
+#include <EVs_DISTNx.h>
+#include <EVs_AngleSensor.h>
+#include <EVs_AbsoluteIMU.h>
+#include <EVShieldUART.h>
+#include <EVShieldI2C.h>
+#include <EVShieldAGS.h>
+#include <BaseI2CDevice.h>
+#include <Wire.h>
+#include <EVShield.h>
+
+
+
+/*
+ Name:		RoboGuy.ino
+ Created:	11/9/2017 5:47:07 PM
+ Author:	bahab
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
