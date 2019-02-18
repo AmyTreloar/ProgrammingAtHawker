9a0fd948288f9999da2d14bfad56762f0db9d0ca
carteras <adam.carter@ed.act.edu.au>
2019-02-18 20:58:44 +1100

    weeeek 3 css

--- 2019/Python/test.py
+++ 2019/Python/test.py
@@ -1,3 +1,7 @@
-foo = "adam carter"
-foo = foo.title()
-print(foo)
+requested_toppings = ['mushrooms', 'green peppers', 'extra cheese']
+for requested_topping in requested_toppings:
+    if requested_topping == 'green peppers':
+        print("Sorry, we are out of green peppers right now")
+    else:
+        print("Adding " + requested_topping + "")
+print("\nFinished making your pizza!")
