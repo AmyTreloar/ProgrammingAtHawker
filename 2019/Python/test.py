b30272a4c870a47076960e3c425da88e66bc6d85
carteras <adam.carter@ed.act.edu.au>
2019-02-19 22:22:01 +1100

    layout control

--- 2019/Python/test.py
+++ 2019/Python/test.py
@@ -1,7 +1,9 @@
-requested_toppings = ['mushrooms', 'green peppers', 'extra cheese']
-for requested_topping in requested_toppings:
-    if requested_topping == 'green peppers':
-        print("Sorry, we are out of green peppers right now")
-    else:
-        print("Adding " + requested_topping + "")
+requested_toppings = ['mushrooms', 'extra cheese']
+if 'mushrooms' in requested_toppings:
+    print("Adding mushrooms.")
+elif 'pepperoni' in requested_toppings:
+    print("Adding pepperoni.")
+elif 'extra cheese' in requested_toppings:
+    print("Adding extra cheese.")
 print("\nFinished making your pizza!")
+
