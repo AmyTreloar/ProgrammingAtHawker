3ca2a671906a5eba879bf3740b35c7b75d6e264b
carteras <adam.carter@ed.act.edu.au>
2019-03-05 21:02:34 +1100

    updated hangman and functionalised it
    Produced code to play a simple game of hangman.

--- 2019/Python/week6_Hangman/hangman.py
+++ 2019/Python/week6_Hangman/hangman.py
@@ -1,23 +1,53 @@
 import random
 
-words = []
-with open('good_words.txt') as fp:
-    words = fp.readlines()
 
-word_number = random.randint(0, len(words))
-answer = words[word_number].strip()
-secret = []
-for i in range(0, len(answer)): 
-    secret.append('_')
-print(answer, secret)
+def init_game():
+    with open('better_words.txt') as fp:
+        words = fp.readlines()
+    rnd_word = random.choice(words).strip()
+    rnd_secret = []
+    for i in range(len(rnd_word)):
+        rnd_secret.append('_')
+    return rnd_word, rnd_secret
 
-guesses_left = 8
-while True:
-    guess = input("Guess a letter: ")
+
+def test_end_conditions(secret, current_fails, max_fails):
+    if '_' not in secret:
+        print("You are the winner!")
+        return False
+    if current_fails >= max_fails:
+        print("You lose, the man is hung!")
+        return False
+    return True
+
+
+def get_guess(answer, secret, max_fails, current_fails):
+    print(f"You have {max_fails - current_fails} guesses left.")
+    guess = input(f"{''.join(secret)} Guess a letter: ")
     guess_count = answer.count(guess)
-    print(answer.find(guess), guess_count)
+    return guess, guess_count
 
-    for i in range(0, len(answer)):
+
+def process_guess(guess, guess_count, answer, secret):
+    if guess_count <= 0:
+        return 1
+
+    for i in range(len(answer)):
         if guess == answer[i]:
             secret[i] = guess
-    print(secret)
+    return 0
+
+
+def main_loop():
+    answer, secret = init_game()
+    playing = True
+    max_fails = 8
+    current_fails = 0
+    print(answer)
+    while playing:
+        guess, guess_count = get_guess(answer, secret, max_fails, current_fails)
+        current_fails += process_guess(guess, guess_count, answer, secret)
+        playing = test_end_conditions(secret, current_fails, max_fails)
+
+
+main_loop()
