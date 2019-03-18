fa0d0e7a6a268603fcbd5598150c09c27c612643
carteras <adam.carter@ed.act.edu.au>
2019-03-19 09:12:35 +1100

    Update hangman_line2.py

--- 2019/Python/week6_Hangman/hangman_line2.py
+++ 2019/Python/week6_Hangman/hangman_line2.py
@@ -1,5 +1,6 @@
 import random
 
+
 def get_word_from_file(file_name):
     '''
     Returns a random word from a file of words. 
@@ -10,6 +11,7 @@ def get_word_from_file(file_name):
     output_word = random.choice(hangman_words).rstrip()
     return output_word
 
+
 def make_hidden_word(word):
     '''
     Creates a hidden word of the same length as the word
@@ -18,12 +20,29 @@ def make_hidden_word(word):
     Return: a string
     '''
     output_word = ''
-    #output_word = '_'*len(word)
+    # output_word = '_'*len(word)
     for letter in word:
         output_word += '_'
     return output_word
 
-random_word = get_word_from_file('good_words.txt')
-hidden_word = make_hidden_word(random_word)
 
-print(random_word, hidden_word)
+def make_guess(hidden_word):
+    """
+    Make guess allows the user to select a letter and
+    and if the letter is available, reveals the letter in
+    the word
+    :param hidden_word: a hidden list of letters. i.e., 'adam' would be seen as "____"
+    :return: a list of hidden letters
+    """
+    guess = input("Guess a letter: ")
+    for i in range(0, len(random_word)):
+        if random_word[i] == guess:
+            hidden_word[i] = guess
+    return hidden_word
+
+
+random_word = get_word_from_file('good_words.txt')
+hidden_word = list(make_hidden_word(random_word))
+while True:
+    print(random_word, ''.join(hidden_word))
+    make_guess(hidden_word)
