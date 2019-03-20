2e1c7737f0d4cd710c5c5186c59caedef570b46f
carteras <adam.carter@ed.act.edu.au>
2019-03-20 15:11:20 +1100

    Update hangman_line4.py

--- 2019/Python/week6_Hangman/hangman_line4.py
+++ 2019/Python/week6_Hangman/hangman_line4.py
@@ -1,10 +1,37 @@
 import random
 
-with open('good_words.txt') as fp:
-    words = fp.readlines()
 
-random_word = random.choice(words).strip()
-hidden_word = ''
-for letter in random_word:
-    hidden_word += '_'
-print(random_word, hidden_word)
+def get_word(file_name):
+    with open(file_name) as fp:
+        words = fp.readlines()
+    return random.choice(words).strip()
+
+
+def get_hidden_word(word):
+    out = ''
+    for letter in word:
+        out += '_'
+    return list(out)
+
+
+random_word = get_word("good_words.txt")
+hidden_word = get_hidden_word(random_word)
+#all letters found!
+#running out of guesses!
+
+guesses=0
+max_guesses = 3
+while guesses <= max_guesses:
+    print(random_word, ''.join(hidden_word), f'Guesses left {max_guesses - guesses}')
+    guess = input("Guess a letter: ")
+    found_letter = False
+    for i in range(0, len(random_word)):
+        if guess == random_word[i]:
+            found_letter = True
+            hidden_word[i] = guess
+
+    if not found_letter:
+        guesses += 1
+
+
+
