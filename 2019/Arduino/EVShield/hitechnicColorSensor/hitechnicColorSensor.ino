0f750a9e66294ca85a5abb2728a76f70bff98fb3
carteras <adam.carter@ed.act.edu.au>
2019-03-29 11:54:16 +1100

    Update hitechnicColorSensor.ino

--- 2019/Arduino/EVShield/hitechnicColorSensor/hitechnicColorSensor.ino
+++ 2019/Arduino/EVShield/hitechnicColorSensor/hitechnicColorSensor.ino
@@ -1,23 +1,29 @@
 #include <Wire.h>
 #include <EVShield.h>
+#include <EVs_NXTTouch.h>
 #include <EVs_HTColorSensor.h>
 
 EVShield evshield(0x34, 0x36);
-EVs_HTColorSensor colorSensor(0x02);
+EVs_NXTTouch touch;
+EVs_HTColorSensor colorSensor1(SH_Bank_A);
+EVs_HTColorSensor colorSensor2(SH_Bank_B);
 
 void setup() {
   Serial.begin(9600);
   Serial.println("---------------");
   Serial.println("Initialising HiTechnic Color Sensor test");
   evshield.init(SH_HardwareI2C);
-  colorSensor.init(&evshield, SH_BAS1);
+  colorSensor1.init(&evshield, SH_BAS2);
+  touch.init(&evshield, SH_BAS1);
+  colorSensor2.init(&evshield, SH_BBS2);
   Serial.println("Press Go button to continue");
-  evshield.waitForButtonPress(BTN_GO);
+  //evshield.waitForButtonPress(BTN_GO);
 
 }
 
 void loop() {
-  Serial.println(colorSensor.getColorId());
+  Serial.print(colorSensor1.getColorId()); Serial.print(" "); Serial.print(colorSensor2.getColorId());
+  Serial.print(" "); Serial.println(touch.isPressed());
   delay(100);
 
 }
