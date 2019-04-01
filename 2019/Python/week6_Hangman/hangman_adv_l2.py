123277bb4e016d5d455feefce2a5b543e170ea89
carteras <adam.carter@ed.act.edu.au>
2019-04-02 09:36:47 +1100

    Update hangman_adv_l2.py

--- 2019/Python/week6_Hangman/hangman_adv_l2.py
+++ 2019/Python/week6_Hangman/hangman_adv_l2.py
@@ -31,21 +31,58 @@ def test_end_conditions(curr_fail, max_fail, chosen, hidden):
         return False
     return True
 
+def get_frequency_analysis(file_name):
+    # todo: the length hidden word
+    # todo: known letters
+    # todo: guesses made
+    letters = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x', 'y','z']
+    letters_count = [0]*len(letters)
+    print(letters)
+    print(letters_count)
+    with open(file_name) as fp:
+        words = fp.readlines()
+    for word in words:
+        word = word.strip()
+        for letter in word:
+            if letter in letters:
+                i = letters.index(letter)
+                letters_count[i] += 1
+            else:
+                letters.append(letter)
+                letters_count.append(1)
+    return letters, letters_count
+
+def next_letter(letters, letters_count):
+    max_number = max(letters_count)
+    i = letters_count.index(max_number)
+    letter = letters[i]
+    letters_count[i] = 0
+    return letter
+
 file_name = 'good_words.txt'
 chosen_word = get_word_from_file(file_name)
 hidden_word = get_hidden_word(chosen_word)
+
 playing = True
 max_fails = 10
 current_fails = 0
 print(chosen_word)
 
+letters, letters_count = get_frequency_analysis(file_name)
+print(letters, len(letters))
+print(letters_count, len(letters_count))
 
+
+#todo: only need to get FA for words this length hidden_word
+#todo: we should update our FA for words that exist with the letters already known
+#todo: e.g., "cat": ___ -> A How many 3 letters have A in the middle?
 while playing:
     print(f'Current guess {"".join(hidden_word)}. You have {max_fails - current_fails} chances left.')
-    guess = input("Guess a letter: ")
+    guess = next_letter(letters, letters_count)
     print(f"You guessed {guess}")
     hidden_word, current_fails = process_guess(chosen_word, hidden_word, guess, current_fails)
     playing= test_end_conditions(current_fails, max_fails, chosen_word, hidden_word)
+    letters, letters_count = get_frequency_analysis(file_name)
     time.sleep(1)
 
 if ''.join(hidden_word) == chosen_word:
