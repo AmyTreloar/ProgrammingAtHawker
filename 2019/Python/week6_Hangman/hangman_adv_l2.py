c27cf4cb0e59123795e2becb5c6569230024e195
carteras <adam.carter@ed.act.edu.au>
2019-04-08 09:29:50 +1000

    Update hangman_adv_l2.py

--- 2019/Python/week6_Hangman/hangman_adv_l2.py
+++ 2019/Python/week6_Hangman/hangman_adv_l2.py
@@ -1,10 +1,12 @@
 import time
 from random import choice
 
-
-def get_word_from_file(filename):
+def get_list_of_words(filename):
     with open(filename) as fp:
         words = fp.readlines()
+    return words
+
+def choose_words_from_list(words):
     return choice(words).strip()
 
 
@@ -31,26 +33,33 @@ def test_end_conditions(curr_fail, max_fail, chosen, hidden):
         return False
     return True
 
-def get_frequency_analysis(file_name):
-    # todo: the length hidden word
-    # todo: known letters
-    # todo: guesses made
+def get_frequency_analysis(list_of_words, hidden_word, guesses):
     letters = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x', 'y','z']
     letters_count = [0]*len(letters)
-    print(letters)
-    print(letters_count)
-    with open(file_name) as fp:
-        words = fp.readlines()
-    for word in words:
+    new_list_of_words = []
+    for word in list_of_words:
         word = word.strip()
+        if len(word) != len(hidden_word):
+            continue
+        new_list_of_words.append(word)
+        for i in range(0, len(hidden_word)):
+            if hidden_word[i] == '_': continue
+            if hidden_word[i] != word[i]:
+                if word in new_list_of_words:
+                    new_list_of_words.remove(word)
+                continue
+
         for letter in word:
-            if letter in letters:
+            if letter in guesses:
+                i = letters.index(letter)
+                letters_count[i] = 0
+            elif letter in letters:
                 i = letters.index(letter)
                 letters_count[i] += 1
             else:
                 letters.append(letter)
                 letters_count.append(1)
-    return letters, letters_count
+    return letters, letters_count, new_list_of_words
 
 def next_letter(letters, letters_count):
     max_number = max(letters_count)
@@ -58,34 +67,37 @@ def next_letter(letters, letters_count):
     letter = letters[i]
     letters_count[i] = 0
     return letter
+win = 0
+lose = 0
+count = 0
+while count <= 1000:
+    file_name = 'good_words.txt'
+    list_of_words = get_list_of_words(file_name)
+    chosen_word = choose_words_from_list(list_of_words)
+    hidden_word = get_hidden_word(chosen_word)
+    guesses = []
+    playing = True
+    max_fails = 10
+    current_fails = 0
+    # print(chosen_word)
 
-file_name = 'good_words.txt'
-chosen_word = get_word_from_file(file_name)
-hidden_word = get_hidden_word(chosen_word)
 
-playing = True
-max_fails = 10
-current_fails = 0
-print(chosen_word)
-
-letters, letters_count = get_frequency_analysis(file_name)
-print(letters, len(letters))
-print(letters_count, len(letters_count))
-
-
-#todo: only need to get FA for words this length hidden_word
-#todo: we should update our FA for words that exist with the letters already known
-#todo: e.g., "cat": ___ -> A How many 3 letters have A in the middle?
-while playing:
-    print(f'Current guess {"".join(hidden_word)}. You have {max_fails - current_fails} chances left.')
-    guess = next_letter(letters, letters_count)
-    print(f"You guessed {guess}")
-    hidden_word, current_fails = process_guess(chosen_word, hidden_word, guess, current_fails)
-    playing= test_end_conditions(current_fails, max_fails, chosen_word, hidden_word)
-    letters, letters_count = get_frequency_analysis(file_name)
-    time.sleep(1)
-
-if ''.join(hidden_word) == chosen_word:
-    print("You are the winner!")
-else:
-    print(f"You are a loser! The word was {chosen_word}")
+    #print(f"So far you have won {win} games and lost {lose} games")
+    playing = True
+    while playing:
+        letters, letters_count, list_of_words = get_frequency_analysis(list_of_words, hidden_word, guesses)
+        #print(f'Current guess {"".join(hidden_word)}. You have {max_fails - current_fails} chances left.', len(list_of_words), win, lose)
+        guess = next_letter(letters, letters_count)
+        guesses.append(guess)
+        #print(f"You guessed {guess}")
+        hidden_word, current_fails = process_guess(chosen_word, hidden_word, guess, current_fails)
+        playing= test_end_conditions(current_fails, max_fails, chosen_word, hidden_word)
+    if ''.join(hidden_word) == chosen_word:
+        # print("You are the winner!")
+        win += 1
+    else:
+        # print(f"You are a loser! The word was {chosen_word}")
+        lose += 1
+    count += 1
+    if count % 100 == 0: print(count, win, lose)
+print(win, lose)
