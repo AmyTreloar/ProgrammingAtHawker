d0611e030e2159bc61c720575fd4f5a1f97bc03d
carteras <adam.carter@ed.act.edu.au>
2019-04-08 10:57:38 +1000

    Update ceaser.py

--- 2019/Python/ciphers/ceaser.py
+++ 2019/Python/ciphers/ceaser.py
@@ -1,10 +1,30 @@
-SYMBOLS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789?!.,'
+SYMBOLS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789?!., '
 ENCRYPT = 0
 DECRYPT = 1
 
 
+def caesar_cipher_lean(msg, key, mode=ENCRYPT):
+    out = ''
+    translated_index = 0
+    if mode == DECRYPT:
+        key = 0 - key
+    for symbol in msg:
+        symbol = symbol.upper().strip()
+        if symbol not in SYMBOLS:
+            continue
+        symbol_index = SYMBOLS.find(symbol)
+        translated_index = symbol_index + key
+        if translated_index >= len(SYMBOLS):
+            translated_index -= len(SYMBOLS)
+        elif translated_index < 0:
+            translated_index += len(SYMBOLS)
+        out += SYMBOLS[translated_index]
+    return out
+
+
 def ceaser_cipher(msg, key, mode=ENCRYPT):
     out = ''
+    translated_index = 0
     for symbol in msg:
         symbol = symbol.upper()
         if symbol not in SYMBOLS:
@@ -24,12 +44,15 @@ def ceaser_cipher(msg, key, mode=ENCRYPT):
     return out
 
 
-
-
-
-# key = 13
-# message = "Three people can keep a secret, if two of them are dead!"
-# cipher = ceaser_cipher(message, key)
+key = 13
+message = "Three people can keep a secret, if two of them are dead!"
+cipher = ceaser_cipher(message, key)
+cipher2 = caesar_cipher_lean(message, key)
+print(message)
+print(cipher)
+print(cipher2)
 # print(cipher)
-# plaintext = ceaser_cipher(cipher, key, DECRYPT)
-# print(plaintext)
+plaintext = ceaser_cipher(cipher, key, DECRYPT)
+plaintext2 = caesar_cipher_lean(cipher2, key, DECRYPT)
+print(plaintext)
+print(plaintext2)
